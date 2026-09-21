e7435c340d86b0340c093ea969aff6a724f51121
Ibnu Arip Alghifari <ibnuarip1601@gmail.com>
2026-09-21 14:36:19 +0700

    feat: apply custom favicon and auth branding

--- resources/js/components/app-logo-icon.tsx
+++ resources/js/components/app-logo-icon.tsx
@@ -1,15 +1,20 @@
 import type { ImgHTMLAttributes } from 'react';
 
-export default function AppLogoIcon({ className, ...props }: ImgHTMLAttributes<HTMLImageElement>) {
+export default function AppLogoIcon({
+    className,
+    ...props
+}: ImgHTMLAttributes<HTMLImageElement>) {
     return (
         <img
             {...props}
             src="/favicon.svg"
             alt={props.alt ?? 'Logo'}
-            className={['block object-contain', className].filter(Boolean).join(' ')}
+            className={['block object-contain', className]
+                .filter(Boolean)
+                .join(' ')}
             style={{
                 display: 'block',
-                ...(props.style ?? {}),
+                ...props.style,
             }}
         />
     );
