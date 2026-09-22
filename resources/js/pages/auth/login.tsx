604a1f9dd88fb8e4e6738e02266b1769ae8474fd
Ibnu Arip Alghifari <ibnuarip1601@gmail.com>
2026-09-22 12:56:43 +0700

    feat: implement login page component

--- resources/js/pages/auth/login.tsx
+++ resources/js/pages/auth/login.tsx
@@ -10,7 +10,6 @@ import { Spinner } from '@/components/ui/spinner';
 import { register } from '@/routes';
 import { store } from '@/routes/login';
 import { request } from '@/routes/password';
-import PasskeyVerify from '@/components/passkey-verify';
 
 type Props = {
     status?: string;
@@ -21,8 +20,6 @@ export default function Login({ status, canResetPassword }: Props) {
     return (
         <>
             <Head title="Log in" />
-
-            <PasskeyVerify />
 
             <Form
                 {...store.form()}
